bb6c32505a7ad525216c237aca26cdd8dfe33ee1
nenjotsu <renenjo.valente@gmail.com>
2020-08-15 23:14:15 +0800

    add docs

--- docs/src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './Content';
-export * from './Main';
-export * from './Nav';
-export * from './InterfaceTable';
-export * from './MethodsTable';
--- docs/src/utils/highlightCode.ts
+++
@@ -1,8 +0,0 @@
-import highlightjs from 'highlight.js';
-
-export function highlightCode() {
-  const blocks = document.querySelectorAll('pre code');
-  Array.from(blocks).map(block => {
-    highlightjs.highlightBlock(block);
-  });
-}
